1352132c0fa5b08eb363b8aecdbbb3015632797e
didac [itb] sanchez cullell <didac.sanchez.7e6@itb.cat>
2023-10-18 15:38:49 +0200

    Parallax Didac
    Parallax. No se ha podido guardar en una rama, así que va al main

--- Assets/Scrips/Test.cs
+++ Assets/Scrips/Test.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEfect;
+
     // Start is called before the first frame update
     void Start()
     {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEfect));
+        float dist = (cam.transform.position.x * parallaxEfect);
+
+        transform.position = new Vector2(startPos + dist, transform.position.y);
+
+        
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
         
     }
 }

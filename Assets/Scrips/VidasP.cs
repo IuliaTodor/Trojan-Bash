3e2d666f8f893f45c46f85dcfb54ba158590f4e5
didac [itb] sanchez cullell <didac.sanchez.7e6@itb.cat>
2023-10-25 16:15:54 +0200

    Vidas
    No morir, triggers i collaiders

---
+++ Assets/Scrips/VidasP.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidasP : MonoBehaviour
+{
+    public int vidas;
+    public GameObject ultimaColl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidas = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Ficheros y Archivos Daño 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.transform.tag);
+        if (collision.transform.tag == "Enemy") 
+        {
+            vidas -= 1;
+        }
+        if (collision.transform.tag == "MainCamera")
+        {
+            vidas -= 1;
+            ultimaColl = collision.gameObject;
+        }
+        if (ultimaColl.tag == "MainCamera" && collision.transform.tag == "Enemy") 
+        {
+            GameObject[] list = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var obj in list)
+            {
+                obj.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+    }
+
+    //No fuera de pantalla
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "MainCamera")
+        {
+            ultimaColl = null;
+        }
+    }
+
+    //Disparos
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            vidas -= 1;
+        }
+    }
+}

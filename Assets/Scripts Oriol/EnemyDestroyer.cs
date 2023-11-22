4b57b0ef70a097427319323d63d75d29d240d358
david [itb] pascal baludo <david.pascal.7e6@itb.cat>
2023-11-22 17:01:48 +0100

    MainSceneUWU

--- Assets/Scripts Oriol/EnemyDestroyer.cs
+++ Assets/Scripts Oriol/EnemyDestroyer.cs
@@ -6,6 +6,6 @@ public class EnemyDestroyer : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy") { Destroy(collision.gameObject); }
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "bicho") { Destroy(collision.gameObject); }
     }
 }

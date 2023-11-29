8642acb6097a103e49af5f80a9e2c1ba7909ae7e
david [itb] pascal baludo <david.pascal.7e6@itb.cat>
2023-11-29 15:26:29 +0100

    Arreglos Tontos

--- Assets/Scripts/Disparo Personaje.cs
+++ Assets/Scripts/Disparo Personaje.cs
@@ -5,28 +5,23 @@ public class ControlDeDisparo : MonoBehaviour
     public GameObject balaPrefab;
     public float velocidadBala = 10f;
     public float cooldownEntreDisparos = 1f;
-    public Animator _animator;
+    public float timeWAait = 111;
 
     private float tiempoUltimoDisparo;
-    private void Start()
-    {
-        _animator = GetComponentInParent<Animator>();
-    }
 
     void Update()
     {
   
         if (Input.GetKey(KeyCode.Space) && Time.time > tiempoUltimoDisparo + cooldownEntreDisparos)
         {
-            _animator.SetBool("Disparo", true);
             Disparar();
         }
+        
     }
     
 
     void Disparar()
     {
-        
         GameObject bala = Instantiate(balaPrefab, transform.position, Quaternion.identity);
 
        
@@ -35,13 +30,10 @@ public class ControlDeDisparo : MonoBehaviour
 
       
         tiempoUltimoDisparo = Time.time;
-
-        //_animator.SetBool("Disparo", false);
     }
+   
 
-
-
-
+    
     void OnBecameInvisible()
     {
         

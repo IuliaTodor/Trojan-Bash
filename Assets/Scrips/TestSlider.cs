933a771d74165030cbcd0a149d6d121fbce53a8f
didac [itb] sanchez cullell <didac.sanchez.7e6@itb.cat>
2023-12-13 16:58:21 +0100

    Ajuestes en pausa

--- Assets/Scrips/TestSlider.cs
+++ Assets/Scrips/TestSlider.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class TestSlider : MonoBehaviour
 {
     public Slider musicSlider;
+    public Slider efectSlider;
+    public AudioManager audioManager;
     // Start is called before the first frame update
     void Start()
     {
-        musicSlider = GetComponent<Slider>();
+        audioManager = GameObject.Find("Test").GetComponent<AudioManager>();//Donde pone Test poner el nombre del Objeto con el componente AudioManager
+        Slider[] sliders = GetComponentsInChildren<Slider>();
+        musicSlider = sliders[0];
         musicSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        efectSlider = sliders[1];
+        efectSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
     }
 
     // Update is called once per frame
     void ValueChangeCheck()
     {
-        Debug.Log(musicSlider.value);
+        if (audioManager != null)
+        {
+            audioManager.musicVolume = musicSlider.value;
+            audioManager.efectVolume = efectSlider.value;
+        }
     }
 }
